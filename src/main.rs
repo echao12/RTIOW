1a1e78d23ff5fb45ceec21abaf3e06b73227862c
ChaoTime <echao0077@gmail.com>
2021-01-09 03:55:04 -0800

    added stderror msgs

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,13 @@ fn main() {
     //structuring .ppm format. P3 means colors in ASCII, then col x row, then max value.
     let mut file = File::create("image.ppm").expect("failed to create file");
     let line: String = format!("P3\n{} {} \n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
-    println!("{}", line);
+    println!("{}", line);//apparently this outputs to standatd output by default
     write!(file, "{}", line);//this macro will format and return a string
     //note, rows written top->bottom and pixels from left -> right
     for j in (0..IMAGE_HEIGHT).rev() {
-        //println!("height:{}", i);
+        eprintln!("\rScanlines remaining: {}", j);//output into standard error stream
         for i in 0..IMAGE_WIDTH {
+
             //top left starts as full green and will travel -> to red in a diagonal.
             let r = (i as f64) / ((IMAGE_WIDTH-1) as f64);//converting to range of 0->1 using denominator to be width or height
             let g = (j as f64) / ((IMAGE_HEIGHT-1) as f64);
@@ -27,5 +28,5 @@ fn main() {
             write!(file,"{} {} {}\n", ir, ig, ib);//cant use '?' in functions that return () values
         }
     }
-    println!("Finished writing to .ppm file...");
+    eprintln!("Finished writing to .ppm file...");
 }
